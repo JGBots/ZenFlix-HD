baabb8cae7a1f33c03aa2f75801fb21ef7739003
JGBots <143808347+JGBots@users.noreply.github.com>
2023-09-11 20:50:36 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+console.log('Iniciando....')
+const express = require('express')
+const cors = require('cors')
+const secure = require('ssl-express-www')
+const PORT = process.env.PORT || 8080 || 5000 || 3000
+const { color } = require('./lib/color.js')
+
+const mainrouter = require('./routes/main')
+const apirouter = require('./routes/api')
+
+const app = express()
+app.enable('trust proxy');
+app.set("json spaces",2)
+app.use(cors())
+app.use(secure)
+app.use(express.static("public"))
+
+app.use('/', mainrouter)
+app.use('/zenflix', apirouter)
+
+app.listen(PORT, () => {
+    console.log(color(`Servidor Conectado Com Sucesso! Rodando Na Porta http://localhost:${PORT}/`, 'green'))
+})
+
+module.exports = app
